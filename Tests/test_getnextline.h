ed785c5aea4c62feec8e39706f2ced322c05df97
sadoming <toraleisha@gmail.com>
2023-09-21 16:20:21 +0200

    Carpeta para los tests

---
+++ Tests/test_getnextline.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_getnextline.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadoming <sadoming@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 17:16:27 by sadoming          #+#    #+#             */
+/*   Updated: 2023/09/21 14:25:16 by sadoming         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_GETNEXTLINE_H
+# define TEST_GETNEXTLINE_H
+
+# include <fcntl.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <string.h>
+# include "../get_next_line.h"
+
+# ifndef TBNS
+#  define TBNS 0
+# endif
+
+size_t	len_tojump(const char *s);
+size_t	my_strlen(const char *s);
+
+int		contains(const char *str, char in);
+int		look4ko(char *you, char *exp);
+int		resume(int len, int *res);
+
+void	sep(char c);
+void	cc(char color);
+#endif
